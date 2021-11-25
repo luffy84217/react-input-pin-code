1688f8704837824fb519ffba280906092e8deea5
Asriel Yu <luffy84217@gmail.com>
2021-11-25 10:16:20 +0800

    chore(storybook): rename title to be more precise

--- src/stories/PinInput.stories.tsx
+++ src/stories/PinInput.stories.tsx
@@ -4,7 +4,7 @@ import PinInput from '../components/PinInput';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/PinInput',
+  title: 'Components/PinInput',
   component: PinInput,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
--- src/stories/StatefulPinInput.stories.tsx
+++ src/stories/StatefulPinInput.stories.tsx
@@ -4,7 +4,7 @@ import StatefulPinInput from '../components/StatefulPinInput';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/StatefulPinInput',
+  title: 'Components/StatefulPinInput',
   component: StatefulPinInput,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
