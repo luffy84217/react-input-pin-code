143b070dc004e71b0aae5ea70ce6a11083d81a55
Asriel Yu <luffy84217@gmail.com>
2021-11-29 21:40:04 +0800

    test: add test case to StatefulPinInput

--- src/tests/StatefulPinInput.test.tsx
+++ src/tests/StatefulPinInput.test.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import StatefulPinInput from '../components/StatefulPinInput';
-import { StatefulPinInputProps } from '../types/StatefulPinInput';
 
 describe('Stateful Pin Input', () => {
-  let props: StatefulPinInputProps;
-
-  beforeEach(() => {
-    props = {
-      length: 4,
-    };
-  });
-
-  const renderComponent = () => render(<StatefulPinInput {...props} />);
-
-  it('should contain 4 inputs', () => {
-    const { getAllByRole } = renderComponent();
-
+  it('should contain 4 inputs by default', () => {
+    const { getAllByRole } = render(<StatefulPinInput />);
     const PinInputFields = getAllByRole('textbox');
 
     expect(PinInputFields).toHaveLength(4);
+  });
+
+  it('should update state on change', () => {
+    const changePin = jest.fn();
+    const { getAllByRole } = render(<StatefulPinInput onChange={changePin} />);
+    const handleChange = jest.spyOn(React, 'useState') as jest.SpyInstance<
+      [string[], React.Dispatch<React.SetStateAction<string[]>>],
+      string[][]
+    >;
+    handleChange.mockImplementation((string) => [string, changePin]);
+
+    const PinInputFields = getAllByRole('textbox') as HTMLInputElement[];
+
+    PinInputFields.forEach((input, i) => {
+      fireEvent.change(input, { target: { value: i } });
+    });
+
+    expect(changePin).toBeTruthy();
   });
 });
